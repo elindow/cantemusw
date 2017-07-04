dee8932f15cefd4b600420656158c3ab78df1fce
Eric Lindow <elindow@verizon.net>
2017-07-04 11:43:46 -0400

    add routine tasks to punch list, add policy files

---
+++ app/policies/song_policy.rb
@@ -0,0 +1,29 @@
+class SongPolicy < ApplicationPolicy
+
+	def show?
+		user?
+	end
+	
+
+	def create?
+		if user.nil? 
+			false
+		else
+			user.admin?
+		end
+	end
+
+	def update?
+		if user.nil? 
+			false
+		else
+			user.admin?
+		end
+	end
+
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+end
