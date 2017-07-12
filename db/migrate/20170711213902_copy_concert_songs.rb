59712bd8e25c8a8f3517c7548b16c842d9686984
Eric Lindow <elindow@verizon.net>
2017-07-12 15:41:50 -0400

    moved HABTM concerts_songs_ to :through programs note copy migration will not work on production

--- db/migrate/20170711213902_copy_concert_songs.rb
+++ db/migrate/20170711213902_copy_concert_songs.rb
@@ -1,20 +1,11 @@
 class CopyConcertSongs < ActiveRecord::Migration[5.0]
-	class Concert < ActiveRecord::Base
-		has_and_belongs_to_many :songs
-		has_many :programs
-		has_many :songs, :through => :programs
-	end
-
-	class Song < ActiveRecord::Base
-		has_and_belongs_to_many :concerts
-		has_many :programs
-		has_many :concerts, :through => :programs
-	end
 
   def change
-  	Concert.find_each do |concert|
-  		puts 
-  		concert.programs = concert.songs
+  	Concert.transaction do
+  		Concert.find_each do |concert|
+  			concert.songs = concert.dep_songs
+  		end
   	end
   end
+
 end
