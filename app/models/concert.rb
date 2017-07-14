0840d9cdfca0186831974c4c609e9eb77f348e03
Eric Lindow <elindow@verizon.net>
2017-07-13 23:55:09 -0400

    temp concerts model to allow migration

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,9 +1,9 @@
 class Concert < ApplicationRecord
 	has_and_belongs_to_many :singers
 
-	#has_and_belongs_to_many :dep_songs,
-	#	join_table: :concerts_songs, class_name: Song,
-	#	association_foreign_key: :song_id
+	has_and_belongs_to_many :dep_songs,
+		join_table: :concerts_songs, class_name: Song,
+		association_foreign_key: :song_id
 
 	has_many :programs
 	has_many :songs, :through => :programs
