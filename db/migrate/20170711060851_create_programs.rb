21ccedde5eececfe9e911065f8bf5dafaac4f7bb
Eric Lindow <elindow@verizon.net>
2017-07-14 00:11:09 -0400

    fix messed up migration

--- db/migrate/20170711060851_create_programs.rb
+++ db/migrate/20170711060851_create_programs.rb
@@ -1,4 +1,4 @@
-class CreateConcerts_Songs_tmp < ActiveRecord::Migration[5.0]
+class CreatePrograms < ActiveRecord::Migration[5.0]
   def change
     create_table :programs do |t|
       t.references :concert, comment: 'concert id for given song'
