8d89feb2a8f16fd186249a9871119b1c32dde45d
Eric Lindow <elindow@verizon.net>
2017-07-14 03:00:02 -0400

    minor bug and UI fixes

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,6 +1,6 @@
 class Program < ApplicationRecord
 	# validations
-	validates_presence_of :concert, :song
+	#validates_presence_of :concert, :song
 
 	#relations
 	belongs_to :concert
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ApplicationRecord
 	has_many :programs
 	has_many :concerts, :through => :programs
-	accepts_nested_attributes_for :programs
+	#accepts_nested_attributes_for :programs
 
 
 	has_and_belongs_to_many :singers
